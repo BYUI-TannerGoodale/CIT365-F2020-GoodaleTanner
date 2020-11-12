618c023c9b4572cf345227ded673b4a750108f3b
Tanner Goodale <goo1811@byui.edu>
2020-11-11 23:45:31 -0600

    Minor bug fixes Version 1.0.2

--- MvcMovie/MvcMovie/wwwroot/js/site.js
+++ MvcMovie/MvcMovie/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('.custom-file-input').on("change", function () {
+        var filename = $(this).val().spilt("\\").pop();
+        $(this).next('.custom-file-label').html(filename);
+    })
+});
