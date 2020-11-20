97d50e8109b97be29444c0b9f2a36facdc2a19ba
Tanner Goodale <goo1811@byui.edu>
2020-11-19 19:54:29 -0600

    Tutorial Compleate Project .NET 5.0 (Bug: Can not edit instrutor, checked against repo code, error in tutorial, revised ack to custom code, error continues)

--- ContosoUniversity/ContosoUniversity/Controllers/CoursesController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/CoursesController.cs
@@ -127,6 +127,24 @@ namespace ContosoUniversity.Controllers
             return View(courseToUpdate);
         }
 
+        public IActionResult UpdateCourseCredits()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateCourseCredits(int? multiplier)
+        {
+            if (multiplier != null)
+            {
+                ViewData["RowsAffected"] =
+                    await _context.Database.ExecuteSqlRawAsync(
+                        "UPDATE Course SET Credits = Credits * {0}",
+                        parameters: multiplier);
+            }
+            return View();
+        }
+
         private void PopulateDepartmentsDropDownList(object selectedDepartment = null)
         {
             var departmentsQuery = from d in _context.Departments
